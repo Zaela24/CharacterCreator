f7bdea395c53217356c3fc02fd2ca810a2190316
Malcolm Grossman <malcolmcgrossman@gmail.com>
2020-05-06 20:42:25 -0500

    Created Character class and filled in some __init__ traits

---
+++ Character/Character.py
@@ -0,0 +1,22 @@
+class Character:
+    """ This Initializes a character object and all of its traits"""
+    def __init__(self):
+        self.str = 0
+        self.dex = 0
+        self.con = 0
+        self.int = 0
+        self.wis = 0
+        self.char = 0
+        self.AC = 0
+        self.HP = 0
+        self.hit_dice = 0
+        self.name = None
+        self.race = None
+        self.class = None
+        self.skills = ()
+        self.religion = None
+        self.alignment = None
+        self.phys_characteristics = ""
+        self.carry_cap = 0
+        self.death_saves = None
+        self.speed
