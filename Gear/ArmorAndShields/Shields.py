3a25a003d614db2d160fbad44eca1eae9bd418e9
Malcolm Grossman <malcolmcgrossman@gmail.com>
2020-05-08 23:57:18 -0500

    added shields init

---
+++ Gear/ArmorAndShields/Shields.py
@@ -0,0 +1,11 @@
+class Shields:
+    """Initialize basic attributes of shields"""
+    def __init__(self):
+        self.name = "" # name of item; example: Heater shield
+        self.ac_bonus = 0
+        self.weight = 0
+        self.price = 0
+        self.condition = "" # Broken, etc.
+        slef.check_penalty = 0 #applies to dex- and str-based skills
+        self.arcane_failure_chance = 0 # percentage
+        self.max_dex_modifer = 0
