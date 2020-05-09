91b6ba098adf038bba7318e7dc8748bfc2a111b7
Zaela24 <zaela.marie.rose@gmail.com>
2020-05-08 22:23:04 -0500

    Implemented __init__ for Weapons & changed SpecialMaterials -> ItemModifiers

---
+++ Gear/ItemModifiers/ItemModifiers.py
@@ -0,0 +1,2 @@
+# To store multiple decorator classes for special materials, magic weapons/armor
+#   weapon/armor special abilities, masterwork items, etc.
--- Gear/Weapons/Weapons.py
+++ Gear/Weapons/Weapons.py
@@ -1,1 +1,15 @@
 # General / abstract class
+class Weapons:
+    """Initializes general Weapon attributes across games and weapon types"""
+    def __init__(self):
+        self.name = "" # item name, example: Dagger
+        self.price = 0 # assuming in gold pieces
+        self.expertise_category = "" # Simple, Martial, Exotic
+        self.handling = "" # Light, One-Handed, Two-Handed
+        self.damage = "" # example, 1d6
+        self.damage_type = "" # Bludgeoning, Piercing, Slashing
+        self.condition = "" # Broken, etc
+        self.size_category = "M" # Small, Medium, Light ; usually Medium
+        self.crit_threat = 20 # usually 20 unless otherwise specified
+        self.crit_modifer = 2 # usually x2 on a crit hit unless otherwise noted
+        self.attack_bonus = 0 # only modified for Masterwork and Magic weapons
