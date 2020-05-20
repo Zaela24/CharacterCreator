25d76deb43ed517fbf00f09fec9a699bce9f66e0
Zaela24 <zaela.marie.rose@gmail.com>
2020-05-20 18:22:23 -0500

    Updated PathfinderCharacter ; commented out 2e Pathfinder class for now

--- Character/PathfinderCharacters.py
+++ Character/PathfinderCharacters.py
@@ -3,6 +3,9 @@ class PathfinderCharacter(Character): # 1e Pathfinder
         self.advancement_track = "" # Fast, Medium, or Slow exp track
         self.feats = dict()
         self.traits = dict()
+        self.will = 0
+        self.ref = 0
+        self.fort = 0
         self.dr = 0
         self.sr = 0
         self.resistences = dict()
@@ -15,10 +18,12 @@ class PathfinderCharacter(Character): # 1e Pathfinder
         self.cmd = 0
         self.touch_ac = 10
         self.flatfoot_ac = 10
+        self.skills = list([]) # create matrix w/ ranks, class bonuses, etc
 
-class Pathfinder2Character(PathfinderCharacter): # 2e Pathfinder
-    def __init__(self):
-        self.ancestry = None
-        self.background = None
-        self.hero_points = 0
-        self.bulk = 0
+## Will only work on 1e Pathfinder for the time being
+# class Pathfinder2Character(PathfinderCharacter): # 2e Pathfinder
+#     def __init__(self):
+#         self.ancestry = None
+#         self.background = None
+#         self.hero_points = 0
+#         self.bulk = 0
