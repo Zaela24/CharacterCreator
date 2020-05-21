848942dea90333253e3b6b1db7cf3fae64963388
Malcolm Grossman <malcolmcgrossman@gmail.com>
2020-05-20 19:05:32 -0500

    added beginning of CharacterClass

---
+++ CharacterClasses/CharacterClasses.py
@@ -0,0 +1,10 @@
+class CharacterClass:
+    """Initializes class for input Character object"""
+    def __init__(self, character, class):
+        """
+        args:
+            character (Character) = An object of the Character class
+            class (str) = A string indicating what class the character should be assinged
+        """
+        self.character = character
+        self.class = class
