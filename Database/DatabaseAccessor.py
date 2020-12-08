61e6891b1b28f91b383cc14d855254c307297016
Malcolm Grossman <malcolmcgrossman@gmail.com>
2020-12-08 00:14:22 -0600

    added character database interactions

--- Database/DatabaseAccessor.py
+++ Database/DatabaseAccessor.py
@@ -1,6 +1,11 @@
 import sqlite3
 
 class CharacterDB:
+    def __init__(self, first_connect):
+        if first_connect:
+            create_db()
+        self.db = self.__enter__()
+
     """Returns database connection that should be used in a with statement"""
     def __enter__(self):
         # Creates database if it does not already exist, otherwise connects to
@@ -13,15 +18,57 @@ class CharacterDB:
         print("connection closed") # debug
         self.database.close()
 
+    def get_character_from_id(self, id):
+        try:
+            execute_string = '''SELECT * from characters WHERE id = %s
+            ''' % (id)
+            character = self.db.execute(execute_string)
+        except Exception as e:
+            print(e)
+            character = None
+        return character
+
+    def get_max_character_id(self):
+        try:
+            id = self.db.execute("MAX(ID) expression")
+        except Exception as e:
+            id = None
+            print(e)
+        return id
+
+    def get_all_characters(self):
+        db = self.__enter__()
+        try:
+            characters = db.execute("SELECT * from characters")
+            print(characters)
+        except Exception as e:
+            print(e)
+
+
+    def create_character(self, name, game, char_class, race, level):
+        id = string(int(get_max_character_id())+1)
+        try:
+            execute_string = '''INSTERT INTO characters \
+            VALUES (%s, %s, %s, %s, %s, %s);
+            ''' % (id, name, game, char_class, race, level)
+        except Exception as e:
+            print(e)
+
+
+
+
 # We will need to figure out what tables we need and just create them when the
 # database is initialized for the first time
 #
 # I'm inclluding the following methods just as practice and to show some examples:
 def create_character_table():
-    with CharacterDB() as database:
+    character_database = CharacterDB(False)
+
+    with character_database.db as database:
         try:
             database.execute('''CREATE TABLE characters
                 (id INT PRIMARY KEY NOT NULL,
+                game TEXT NOT NULL,
                 name TEXT NOT NULL,
                 class TEXT NOT NULL,
                 race TEXT NOT NULL,
@@ -34,48 +81,65 @@ def create_character_table():
                 print(e)
 
 def insert_characters():
-    with CharacterDB() as database:
+    character_database = CharacterDB(False)
+
+    with character_database.db as database:
 
         database.execute("INSERT INTO characters \
-            VALUES (1,'Phil','Barbarian','Half-Orc',5);")
+            VALUES (1, 'DnD', 'Phil','Barbarian','Half-Orc',5);")
 
         database.execute("INSERT INTO characters \
-            VALUES (2,'Chuck','Bard','Human',4);")
+            VALUES (2, 'Pathfinder', 'Chuck','Bard','Human',4);")
 
         database.execute("INSERT INTO characters \
-            VALUES (3,'Bartholomew','Rogue','Half-Elf',7);")
+            VALUES (3, 'Other', 'Bartholomew','Rogue','Half-Elf',7);")
 
         database.commit()
         print("Characters added successfully")
 
 def select_characters():
-    with CharacterDB() as database:
-        cursor = database.execute("SELECT id, name, class, level FROM characters;")
+    character_database = CharacterDB(False)
+
+    with character_database.db as database:
+        cursor = database.execute("SELECT id, game, name, class, level FROM characters;")
 
         for row in cursor:
             print("ID =", row[0])
-            print("NAME =", row[1])
-            print("CLASS =", row[2])
-            print("LEVEL =", row[3], "\n")
+            print("GAME =", row[1])
+            print("NAME =", row[2])
+            print("CLASS =", row[3])
+            print("LEVEL =", row[4], "\n")
+
+def get_characters():
+    character_db = CharacterDB(False)
+    character_db.get_all_characters()
 
 def update_characters():
-    with CharacterDB() as database:
+    character_database = CharacterDB(False)
+
+    with character_database.db as database:
         database.execute("UPDATE characters SET level = 6 WHERE name = 'Phil';")
         database.commit()
         print("Total number of rows updated:", database.total_changes)
         select_characters()
 
 def delete_characters():
-    with CharacterDB() as database:
+    character_database = CharacterDB(False)
+
+    with character_database.db as database:
         database.execute("DELETE FROM characters WHERE name = 'Chuck';")
         database.commit()
         select_characters()
 
 def cleanup():
-    with CharacterDB() as database:
+    character_database = CharacterDB(False)
+
+    with character_database.db as database:
         database.execute("DROP TABLE characters")
 
+
 def example_run():
+
     print("\n===CREATE TABLE===\n")
     create_character_table()
     print("\n===INSERT CHARACTERS===\n")
@@ -84,10 +148,14 @@ def example_run():
     select_characters()
     print("\n===UPDATE CHARACTERS===\n")
     update_characters()
+    print("\n===GET ALL CHARACTERS===\n")
+    get_characters()
     print("\n===DELETE CHARACTERS===\n")
     delete_characters()
     cleanup()
     print("\ndone.")
+
+
 
 if __name__ == "__main__":
     example_run()
